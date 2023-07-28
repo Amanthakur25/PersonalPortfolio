1d536be492c3963146f8a2b1bf63699820d0b1af
Aman Singh Thakur <amansinghthakur252002@gmail.com>
2023-07-28 19:55:01 +0530

    "final"

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,98 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from "./resume.pdf";
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>About </span>
+        <span>Me</span>
+        <spane>
+        Welcome to my portfolio website! I'm Aman Singh Thakur, <br />
+        a passionate MERN (MongoDB, Express.js, React.js, Node.js) <br />
+         I enjoy crafting elegant user 
+          interfaces using React.js and  <br /> creating robust backend systems using
+          Node.js and Express.js. <br /> Leveraging the power of MongoDB, I design
+          efficient and  <br />scalable databases to ensure seamless data management.
+          <br />
+           I am also adept at
+          solving algorithmic challenges using <br/>C++, Java, and Python. <br />
+           I believe
+          that a strong foundation in <br/>data structures and algorithms is <br />
+          essential for <br/>building efficient and optimized software solutions
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Languages"}
+            detail={"C++, JAVA, Python,JavaScript,MongoDb,SQL"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={" Web Developer"}
+            detail={"Html, Css, JavaScript, React, Nodejs, Express,MongoDb"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"UI/UX"}
+            detail={
+              " I enjoy crafting elegant user interfaces and my skills in building dynamic and responsive web applications"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
